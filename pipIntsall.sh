4870d10a17ba680d9942d21982deccca6e245281
Masupha <thekomasupha@gmail.com>
2023-10-23 12:30:47 +0200

    Add files via upload

--- pipIntsall.sh
+++ pipIntsall.sh
@@ -1,9 +1,6 @@
 sudo apt install python3-pip
 sudo pip install gspread
 sudo pip install openpyxl
-sudo mv bin.tar.gz /usr/local/bin
+sudo cp bin.tar.gz /usr/local/bin
 cd /usr/local/bin
 sudo tar -xvpzf bin.tar.gz
-cd bin
-sudo mv * ../
-sudo rm -rf /usr/local/bin/bin
